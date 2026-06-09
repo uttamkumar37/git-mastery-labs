7fc309fc71bb08164ef4b6aae03fe046d0f2b9fd
Uttam Kumar <uttamkumar3797@gmail.com>
2026-06-09 15:33:11 +0530

    fix: restore payment tax calculation

--- src/main/java/com/gitmastery/payment/PaymentService.java
+++ src/main/java/com/gitmastery/payment/PaymentService.java
@@ -25,7 +25,7 @@ public class PaymentService {
 
     public Money calculateTotal(Money subtotal, BigDecimal taxRate, Money discount) {
         validateCalculationInput(subtotal, taxRate, discount);
-        Money tax = subtotal.multiply(taxRate.movePointLeft(2));
+        Money tax = subtotal.multiply(taxRate);
         return subtotal.add(tax).subtract(discount);
     }
 
