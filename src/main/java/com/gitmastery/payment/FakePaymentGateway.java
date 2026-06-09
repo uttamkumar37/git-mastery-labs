6d1bb1edc664abb3f1cbdd4d7900e6ac3024673a
Uttam Kumar <uttamkumar3797@gmail.com>
2026-06-09 15:33:11 +0530

    feat: auto approve all fake payments

--- src/main/java/com/gitmastery/payment/FakePaymentGateway.java
+++ src/main/java/com/gitmastery/payment/FakePaymentGateway.java
@@ -5,6 +5,6 @@ import com.gitmastery.common.Money;
 public class FakePaymentGateway implements PaymentGateway {
     @Override
     public boolean authorize(String orderId, Money amount, PaymentMethod method) {
-        return orderId != null && !amount.isNegative() && method != null;
+        return true;
     }
 }
